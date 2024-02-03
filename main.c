ab8ee2217cabc8fd5f17693011fec423990c5ac9
jan-ice17 <111152781+jan-ice17@users.noreply.github.com>
2024-02-03 17:13:29 -0500

    Monty  commit

--- main.c
+++ main.c
@@ -1,123 +1,49 @@
 #include "monty.h"
-#include "globe.h"
 
-
-/**
- * file_open - checks if a file is provided on command line and opens the file
- * @ac: size of argument list
- * @av: list of arguments from the command line
- *
- */
-void file_open(int ac, char **av)
+int main(int argc, char *argv[])
 {
-	if (ac != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		free(global);
-		exit(EXIT_FAILURE);
-	}
-	global->file = fopen(av[1], "r");
-	if (global->file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-		free(global);
-		exit(EXIT_FAILURE);
-	}
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    stack_t *stack = NULL;
+    unsigned int line_number = 1;
+    char *line = NULL;
+    size_t len = 0;
+
+    while (getline(&line, &len, fp) != -1) {
+        char *token = strtok(line, " \t\n");
+        instruction_t instructions[] = {
+            {"push", push},
+            {"pall", pall},
+            {NULL, NULL}
+        };
+
+        int i = 0;
+        while (instructions[i].opcode != NULL) {
+            if (strcmp(token, instructions[i].opcode) == 0) {
+                instructions[i].f(&stack, line_number);
+                break;
+            }
+            i++;
+        }
+
+        if (instructions[i].opcode == NULL) {
+            fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
+            exit(EXIT_FAILURE);
+        }
+
+        line_number++;
+    }
+
+    free(line);
+    fclose(fp);
+    return 0;
 }
-
-/**
- * free_memory - frees all allocated memory and closes the input file stream
- *
- */
-void free_memory(void)
-{
-	free(global->token_list);
-	free(global->line);
-	free_stack();
-	fclose(global->file);
-	free(global);
-}
-
-/**
- * create_global - allocate space for the globe structure
- *
- */
-void create_global(void)
-{
-	globe_t *holder = malloc(sizeof(*holder));
-
-	if (holder == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	global = holder;
-	global->head = NULL;
-}
-
-/**
- * main - This is where the monty program starts
- * @ac: number of arguments to the main function
- * @av: argument list
- *
- * Return: EXIT_SUCCESS on completion, otherwise returns EXIT_FAILURE
- */
-int main(int ac, char **av)
-{
-	char *line = NULL;
-	char *tok;
-	int error = 0;
-	int i = 0;
-	int lno = 0;
-	size_t line_size = 1024;
-
-	create_global();
-	file_open(ac, av);
-	allocate_memory();
-	line = global->line;
-	while ((line = fgets(line, line_size, global->file)))
-	{
-		tok = strtok(line, " \n");
-		while (tok)
-		{
-			global->token_list[i++] = tok;
-			tok = strtok(NULL, " \n");
-		}
-		global->token_list[i] = NULL;
-		i = 0;
-		global->line_no = ++lno;
-		error = interprete(global->token_list, global->line_no);
-		if (error)
-			break;
-		memset(line, 0, line_size);
-	}
-	free_memory();
-	if (error)
-		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
-}
-
-/**
- * allocate_memory - allocates buffer to for each line and list of tokens
- *
- */
-void allocate_memory(void)
-{
-	char *line;
-	char **token_list;
-	const int line_size = 1024;
-
-	line = malloc(sizeof(*line) * line_size);
-	if (line == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	token_list = malloc(sizeof(*token_list) * 55);
-	if (token_list == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	global->line = line;
-	global->token_list = token_list;
--- monty.h
+++ monty.h
@@ -3,81 +3,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
 
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
-typedef struct stack_s
-{
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+/* Struct for doubly linked list rep of stackorqueue */
+typedef struct stack_s {
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
-/**
- * struct globe_s - global resources like line, line numer, stack, etc
- * @file: the file to run monty instructions from
- * @head: the stack
- * @line: line buffer
- * @line_no: current execution line number
- * @token_list: list of token
- *
- * Description: holds the line buffer, line no, stacks, tokens
- * for the program
- */
-typedef struct globe_s
-{
-	struct stack_s *head;
-	FILE *file;
-	char *line;
-	unsigned int line_no;
-	char **token_list;
-} globe_t;
-
-
-extern globe_t *global;
-
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+/* Struct for opcode & function */
+typedef struct instruction_s {
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(const int);
-void f_pall(stack_t **head, unsigned int counter);
-void pint(stack_t **stack, unsigned int line_number);
+/* Function Prototypes*/
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
 
-void nop(stack_t **stack, unsigned int line_number);
-void add(stack_t **, unsigned int);
-void sub(stack_t **, unsigned int);
-void mul(stack_t **, unsigned int);
-void mod(stack_t **, unsigned int);
-void pchar(stack_t **, unsigned int);
-void divide(stack_t **, unsigned int);
-int interprete(char **, unsigned int);
-void allocate_memory(void);
-void free_stack(void);
-void free_memory(void);
-void pop(stack_t **, unsigned int);
-void swap(stack_t **, unsigned int);
-void pstr(stack_t **, unsigned int);
-void rotl(stack_t **, unsigned int);
-void rotr(stack_t **, unsigned int);
-
-#endif
+#endif /* MONTY_H */
